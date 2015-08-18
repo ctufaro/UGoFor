95f2afc1df5f6021439f54f39c861d3de1162b31
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-08-18 15:51:18 -0400

    New seed

--- UGoFor.API/DAL/PostsDAL.cs
+++ UGoFor.API/DAL/PostsDAL.cs
@@ -72,9 +72,9 @@ namespace UGoFor.API.DAL
 
             SqlParameter[] parameters = new SqlParameter[]
             {
-                new SqlParameter("@USERID", new Random().Next(1,4)),
+                new SqlParameter("@USERID", new Random(Guid.NewGuid().GetHashCode()).Next(1,4)),
                 new SqlParameter("@SHORTCOMMENT", sentPost.SmallComment),
-                new SqlParameter("@IMAGEURL", "http://ugofor.azurewebsites.net/Content/img/" + sampleFoods[new Random().Next(0,3)]),
+                new SqlParameter("@IMAGEURL", "http://ugofor.azurewebsites.net/Content/img/" + sampleFoods[new Random(Guid.NewGuid().GetHashCode()).Next(0,3)]),
                 new SqlParameter("@COMMENT", sentPost.BigComment),
                 new SqlParameter("@LOCATION", sentPost.Location),
             };
