a2dcf254046b0cdea47295d302029ea1ac2660d2
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-08-24 12:05:22 -0400

    Stubbing InsertNewUser

---
+++ UGoFor.API/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using UGoFor.API.DAL;
+
+namespace UGoFor.API.Models
+{
+    public class RegisterModel
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public void InsertNewUser(RegisterModel sentRegister)
+        {
+            new RegisterDAL().InsertNewUser(sentRegister);
+        }
+    }
+}
