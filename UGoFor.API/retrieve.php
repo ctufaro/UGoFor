97264f07f7df5f6662ee8f75f8696dfe6cc53b66
christufaro <ctufaro@gmail.com>
2015-09-04 05:39:59 -0400

    Dummy file

--- UGoFor.API/retrieve.php
+++ UGoFor.API/retrieve.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
+  $data = file_get_contents('php://input');
+  header("Location: http://ugoforapi.azurewebsites.net/home/udid?data=".rawurlencode($data));
 ?>
-
-</body>
-</html>
