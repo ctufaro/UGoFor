f763be97791a62bf571039daa02ebd641ec15309
christufaro <ctufaro@gmail.com>
2015-09-04 05:16:53 -0400

    Dummy file

--- UGoFor.API/retrieve.php
+++ UGoFor.API/retrieve.php
@@ -1,4 +1,4 @@
 <?php
   $data = file_get_contents('php://input');
-  header("Location: http://www.google.com/Results?data=".rawurlencode($data));
+  header("Location: http://ugoforapi.azurewebsites.net/home/udid?data=".rawurlencode($data));
 ?>
