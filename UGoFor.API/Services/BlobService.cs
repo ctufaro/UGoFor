e252fbb8b8b582f98e9ec24c72e3b27294dd69d2
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-10-16 16:13:08 -0400

    Adding filters to streams

--- UGoFor.API/Services/BlobService.cs
+++ UGoFor.API/Services/BlobService.cs
@@ -173,10 +173,10 @@ namespace UGoFor.API.Services
                     //APPLY FILTERS HERE                   
 
                     //convert stream into image, and apply filter                    
-                    Image filterApplied = UGoFilters.ApplyFilter(UGoFilters.Filters.Inkwell, Image.FromStream(fs));
+                    //Image filterApplied = UGoFilters.ApplyFilter(UGoFilters.Filters.Inkwell, Image.FromStream(fs));
 
                     //convert compressed image back into stream
-                    Compression.SaveJpegToStreamWithCompressionSetting(filterApplied, 40, fs);
+                    //Compression.SaveJpegToStreamWithCompressionSetting(filterApplied, 40, fs);
 
                     blob.UploadFromStream(fs);
                 }
