9400d7373d6ecb04d266b35f5dee92874e08bf2a
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-12-07 10:40:03 -0500

    Oops, Elmo didnt, make it to the potty, Elmo had an accident but he won't make it next time.

---
+++ UGoFor.API/DAL/CraveDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using UGoFor.API.Models;
+
+namespace UGoFor.API.DAL
+{
+    public class CraveDAL : BaseDAL
+    {
+        public void InsertCrave(CraveModel crave)
+        {
+            //SqlParameter[] parameters = new SqlParameter[]
+            //{
+            //    new SqlParameter("@USERID", crave.UserId),
+            //    new SqlParameter("@SHORTCOMMENT", photoPost.SmallComment),
+            //    new SqlParameter("@COMMENT", photoPost.BigComment),
+            //    new SqlParameter("@LOCATION", photoPost.Location),
+            //    new SqlParameter("@GUID", photoPost.Guid),
+            //};
+
+            //ExecuteSPNonReturnData("InsertUserCrave", parameters);
+        }
+    }
+}
---
+++ UGoFor.API/Models/CraveModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UGoFor.API.DAL;
+
+namespace UGoFor.API.Models
+{
+    public class CraveModel
+    {
+        public int UserId { get; set; }
+        public int? PostId { get; set; }
+        public string CravingText { get; set; }
+        public string CravingAmt { get; set; }
+        public string CravingPic { get; set; }
+        public string TimePosted { get; set; }
+        public string Location { get; set; }
+
+        public void InsertCrave(CraveModel crave)
+        {
+            return new CraveDAL().InsertCrave(crave);
+        }
+    }
+}
