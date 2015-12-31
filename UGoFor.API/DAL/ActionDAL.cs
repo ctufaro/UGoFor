386f4e74f02e342a7847fd63740ded8fcf4f3f56
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-12-31 15:31:11 -0500

    ugoforapi.azurewebsites.net/api/action

---
+++ UGoFor.API/DAL/ActionDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using UGoFor.API.Models;
+
+namespace UGoFor.API.DAL
+{
+    public class ActionDAL : BaseDAL
+    {
+        public void InsertAction(ActionModel sentAction)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@USERID", sentAction.UserId),
+                new SqlParameter("@ACTION", sentAction.Action),
+                new SqlParameter("@VALUE", sentAction.Value),
+            };
+
+            ExecuteSPNonReturnData("InsertAction", parameters);
+        }
+    }
+}
--- UGoFor.API/Models/ActionModel.cs
+++ UGoFor.API/Models/ActionModel.cs
@@ -4,14 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using UGoFor.API.DAL;
+using System.Data;
 
 namespace UGoFor.API.Models
 {
-    public class ActionModel
+    public class ActionModel : IFromDataReader<ActionModel>
     {
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
         public string Action { get; set; }
         public string Value { get; set; }
 
+        public void InsertAction(ActionModel sentAction)
+        {
+            new ActionDAL().InsertAction(sentAction);
+        }
+
+        public ActionModel FromDataReader(IDataReader dr)
+        {
+            ActionModel actionModel = new ActionModel();
+            actionModel.UserId = dr["UserId"] is DBNull ? null : dr["UserId"] as Int32?;
+            actionModel.Action = dr["Action"] is DBNull ? null : dr["Action"].ToString();
+            actionModel.Value = dr["Value"] is DBNull ? null : dr["Value"].ToString();
+            return actionModel;
+        }
     }
 }
