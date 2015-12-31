21644d4c4c2ce99685fb0953a54e4b0e97296289
unknown <ctufaro@VM-DEV-DEV3N.mhtny.com>
2015-12-31 09:24:41 -0500

    Adding action model grrrrrr

---
+++ UGoFor.API/Models/ActionModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UGoFor.API.DAL;
+
+namespace UGoFor.API.Models
+{
+    public class ActionModel
+    {
+        public int UserId { get; set; }
+        public string Action { get; set; }
+        public string Value { get; set; }
+
+    }
+}
